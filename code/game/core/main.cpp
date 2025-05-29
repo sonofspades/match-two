c7e545708d5d0e087570950a0ff2bf8977317bd0
iamdramaticleo <iamdramaticleo@gmail.com>
2025-05-29 20:27:03 +0300

    Add card rendering pipeline and update window resolution
    Integrated rendering logic for card objects using VAOs, VBOs, and UBOs. Introduced camera and material buffers, depth testing, and culling. Updated window resolution to 1920x1080 for improved aspect ratio.

--- code/game/core/main.cpp
+++ code/game/core/main.cpp
@@ -1,10 +1,15 @@
 #include <core/file.hpp>
 
+#include <opengl/buffer.hpp>
 #include <opengl/commands.hpp>
 #include <opengl/functions.hpp>
+#include <opengl/pipeline.hpp>
 #include <opengl/shader.hpp>
+#include <opengl/vertex_array.hpp>
 
+#include <opengl/constants/buffer.hpp>
 #include <opengl/constants/commands.hpp>
+#include <opengl/constants/pipeline.hpp>
 #include <opengl/constants/shader.hpp>
 
 #include <shaders/converter.hpp>
@@ -13,8 +18,8 @@ auto main() -> int32_t
 {
     shaders::Converter::convert("../../resources/shaders", "./");
 
-    constexpr auto window_width  = 1000;
-    constexpr auto window_height = 1000;
+    constexpr auto window_width  = 1920;
+    constexpr auto window_height = 1080;
     static    auto window_closed = false;
 
     if (glfwInit() != GLFW_TRUE)
@@ -74,6 +79,53 @@ auto main() -> int32_t
         }
     }
 
+    constexpr core::vertex_array::attribute position_attribute { 0, 3, opengl::constants::float_type, 0 };
+
+    opengl::Buffer card_vbo;
+    card_vbo.create();
+    card_vbo.storage(core::buffer::make_data(card_vertices));
+
+    opengl::Buffer card_ebo;
+    card_ebo.create();
+    card_ebo.storage(core::buffer::make_data(card_elements));
+
+    opengl::VertexArray card_vao;
+    card_vao.create();
+    card_vao.attach_vertices(card_vbo, sizeof(glm::vec3));
+    card_vao.attach_elements(card_ebo);
+
+    card_vao.attribute(position_attribute);
+
+    glm::mat4 model { 1.0f };
+
+    glm::vec3 material_albedo { 1.0f, 0.0f, 0.0f };
+
+    auto proj = glm::perspective(glm::radians(60.0f), static_cast<float>(window_width) / static_cast<float>(window_height), 0.1f, 100.0f);
+    auto view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -8.0f));
+
+    const std::vector camera_uniforms
+    {
+        view, proj
+    };
+
+    opengl::Buffer transform_ubo;
+    transform_ubo.create();
+    transform_ubo.storage(core::buffer::make_data(&model), opengl::constants::dynamic_draw);
+    transform_ubo.bind_base(opengl::constants::uniform_buffer, core::buffer::transform);
+
+    opengl::Buffer camera_ubo;
+    camera_ubo.create();
+    camera_ubo.storage(core::buffer::make_data(camera_uniforms), opengl::constants::dynamic_draw);
+    camera_ubo.bind_base(opengl::constants::uniform_buffer, core::buffer::camera);
+
+    opengl::Buffer material_ubo;
+    material_ubo.create();
+    material_ubo.storage(core::buffer::make_data(&material_albedo), opengl::constants::dynamic_draw);
+    material_ubo.bind_base(opengl::constants::uniform_buffer, core::buffer::material);
+
+    opengl::Pipeline::enable(opengl::constants::depth_test);
+    opengl::Pipeline::enable(opengl::constants::cull_face);
+
     opengl::Commands::clear(0.5f, 0.5f, 0.5f);
 
     while (!window_closed)
@@ -81,6 +133,24 @@ auto main() -> int32_t
         glfwPollEvents();
 
         opengl::Commands::clear(opengl::constants::color_buffer | opengl::constants::depth_buffer);
+
+        base_shader.bind();
+
+        card_vao.bind();
+
+        for (auto row = 0; row < 13; row++)
+        {
+            constexpr auto tile_size = 1.25f;
+
+            const auto x = tile_size * row - 6.0f * tile_size;
+            const auto y = 0.0f;
+
+            model = glm::translate(glm::mat4(1.0f), glm::vec3(x, y, 0.0f));
+
+            transform_ubo.update(core::buffer::make_data(&model));
+
+            opengl::Commands::draw_elements(opengl::constants::triangles, card_elements.size());
+        }
 
         glfwSwapBuffers(window);
     }
